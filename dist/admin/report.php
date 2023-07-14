eca68335d758afcc6e0300a5edff55928edd0948
arianne05 <quimpoarianne5@gmail.com>
2023-07-14 18:19:41 +0800

    Changed html to php extension

---
+++ dist/admin/report.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Links -->
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="https://kit.fontawesome.com/8b614ed6c5.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+  </head>
+  <body>
+    <!-- Topbar -->
+    <?php include_once '../navbar/topbar.php'?>
+    <!-- Sidebar -->
+    <?php include_once '../navbar/sidebar.php'?>
+    
+    <div class="reports-total-container">
+      <div class="total-user">
+        <p>124</p>
+        <p>Total Users</p>
+      </div>
+      <div class="total-student">
+        <p>124</p>
+        <p>Total Student</p>
+      </div>
+      <div class="total-teacher">
+        <p>124</p>
+        <p>Total Teachers</p>
+      </div>
+    </div>
+
+  </body>
+</html>
