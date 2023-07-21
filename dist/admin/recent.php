1ca51e112d927398308f843c5aeb5c94fecb6f92
arianne05 <quimpoarianne5@gmail.com>
2023-07-21 11:12:48 +0800

    Added active link in sidebar href, add section for recent

---
+++ dist/admin/recent.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links -->
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="https://kit.fontawesome.com/8b614ed6c5.js" crossorigin="anonymous"></script>
+    <title>Recent | Admin</title>
+</head>
+<body>
+    <!-- Topbar -->
+    <?php include_once '../navbar/topbar.php'?>
+    <!-- Sidebar -->
+    <?php include_once '../navbar/sidebar.php'?>
+
+    <!-- Recent Body -->
+    <div class="recent-main-container">
+        <div class="recent-main-header">
+            <h3>Recent Activities</h5>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, architecto.</p>
+        </div>
+        <div class="recent-faculty-member">
+            <div class="user-activity">
+                <h3>User Activity</h3>
+                <p>lorem</p>
+            </div>
+            <div class="list-member">
+                <h3>Faculty Member</h3>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
