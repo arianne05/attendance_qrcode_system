e34c4fed4e9088cd91a08d834c3b26961428a7a2
arianne05 <quimpoarianne5@gmail.com>
2023-07-21 17:57:52 +0800

    Added design for user activity and faculty member

--- dist/admin/recent.php
+++ dist/admin/recent.php
@@ -34,17 +34,67 @@
                     <button>Sort</button>
                 </div>
             </div>
+
+            <!-- Middle Section -->
             <div class="table-container">
-                    <p>sda</p>
-                </div>
+                <table>
+                    <tr>
+                        <th>Professor Name</th>
+                        <th>Action</th>
+                        <th>Student Name</th>
+                        <th>Date</th>
+                    </tr>
+                    <tr>
+                        <td>Arianne H. Quimpo</td>
+                        <td>Added</td>
+                        <td>Elrich Lanuza</td>
+                        <td>10:04 AM Feb 23, 2023</td>
+                    </tr>
+                </table>
+            </div>
         </div>
+
+        <!-- Right Section -->
         <div class="recent-faculty-member">
             <div class="user-activity">
                 <h3>User Activity</h3>
-                <p>lorem</p>
+                <div class="add-container">
+                    <div class="label">
+                        <p class="add"></p>
+                        <p>Added</p>
+                    </div>
+                    <p>10</p>
+                </div>
+                <div class="edit-container">
+                    <div class="label">
+                        <p class="edit"></p>
+                        <p>Edited</p>
+                    </div>
+                    <p>3</p>
+                </div>
+                <div class="archive-container">
+                    <div class="label">
+                        <p class="archive"></p>
+                        <p>Archived</p>
+                    </div>
+                    <p>20</p>
+                </div>
             </div>
+
             <div class="list-member">
                 <h3>Faculty Member</h3>
+                <div class="member-container">
+                    <p class="circle-user-recent">AQ</p>
+                    <p>Arianne Quimpo</p>
+                </div>
+                <div class="member-container">
+                    <p class="circle-user-recent">EL</p>
+                    <p>Elrich Lanuza</p>
+                </div>
+                <div class="member-container">
+                    <p class="circle-user-recent">RC</p>
+                    <p>Rens Campos</p>
+                </div>
             </div>
         </div>
     </div>
