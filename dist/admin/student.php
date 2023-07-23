353feb9582f2f6f2adde6be5ee36b2a4b6971037
arianne05 <quimpoarianne5@gmail.com>
2023-07-23 16:20:40 +0800

    Added student file for admin

---
+++ dist/admin/student.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links -->
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="https://kit.fontawesome.com/8b614ed6c5.js" crossorigin="anonymous"></script>
+    <title>Student | Admin</title>
+</head>
+<body>
+    <!-- Topbar -->
+    <?php include_once '../navbar/topbar.php'?>
+    <!-- Sidebar -->
+    <?php include_once '../navbar/sidebar.php'?>
+</body>
+</html>
