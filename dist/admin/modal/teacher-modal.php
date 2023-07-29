6b519bd63af23079f8af7d13f9b772a3f5d9a839
arianne05 <quimpoarianne5@gmail.com>
2023-07-29 19:27:36 +0800

    Updated form for add new teacher

--- dist/admin/modal/teacher-modal.php
+++ dist/admin/modal/teacher-modal.php
@@ -49,19 +49,108 @@
             <div id="sectionContainer">
             <!-- Initial section -->
             <div class="section-three-addTeacher">
+                <!-- SCHEDULE -->
                 <div class="bind-label">
-                    <label for="teacherSchedule1">Schedule:</label>
-                    <input type="text" id="teacherSchedule1" name="teacherSchedule1">
+                    <label for="schedule">Schedule</label>
+
+                    <div class="sched-container">
+                        <!-- From Label -->
+                        <div class="from-label">
+                            <label>From:</label> <!-- Label -->
+                            <div class="main-select-container"> <!-- Selects -->
+                                <!-- Select 1 -->
+                                <div class="select-container">
+                                    <select name="schedFromHour1">
+                                    <?php for($i=1; $i<=12; $i++){ ?>
+                                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                    <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- Select 2 -->
+                                <div class="select-container">
+                                    <select name="schedFromMin1">
+                                        <?php for($i=1; $i<=59; $i++){ ?>
+                                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- Select 3 -->
+                                <div class="select-container">
+                                    <select name="schedFromPeriod1">
+                                        <option value="AM">AM</option>
+                                        <option value="PM">PM</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- To Label -->
+                        <div class="from-label">
+                            <label for="teacherSchedule1">To:</label> <!-- Label -->
+                            <div class="main-select-container"> <!-- Selects -->
+                                <!-- Select 1 -->
+                                <div class="select-container">
+                                    <select name="schedToHour1">
+                                    <?php for($i=1; $i<=12; $i++){ ?>
+                                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                    <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- Select 2 -->
+                                <div class="select-container">
+                                    <select name="schedToMin1">
+                                        <?php for($i=1; $i<=59; $i++){ ?>
+                                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- Select 3 -->
+                                <div class="select-container">
+                                    <select name="schedToPeriod1">
+                                        <option value="AM">AM</option>
+                                        <option value="PM">PM</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <!-- GRADE -->
                 <div class="bind-label">
-                    <label for="teacherSection1">Section Name:</label>
+                    <br>
+                    <label>Grade:</label>
+                    <input type="text" id="teacherGrade1" name="teacherGrade1">
+                </div>
+                <!-- SECTION -->
+                <div class="bind-label">
+                    <br>
+                    <label>Section:</label>
                     <input type="text" id="teacherSection1" name="teacherSection1">
                 </div>
+                <!-- SUBJECT -->
                 <div class="bind-label">
-                    <label for="teacherSubject1">Subject Name:</label>
+                    <br>
+                    <label>Subject Name:</label>
                     <input type="text" id="teacherSubject1" name="teacherSubject1">
                 </div>
+
+                <!-- School Year -->
+                <div class="bind-label">
+                    <label>School Year:</label> <!--Label-->
+                    <div class="sched-container"> <!--School Year-->
+                        <div class="from-label">
+                            <label>From:</label>
+                            <input type="number" id="teacherSchedule1" name="teacherFromSchoolYear1">
+                        </div>
+                        <div class="from-label">
+                            <label>To:</label>
+                            <input type="number" id="teacherSchedule1" name="teacherToSchoolYear1">
+                        </div>
+                    </div>
+                </div>
                 <div class="withButton">
+                    <br>
                     <label for="teacherSubject1">d</label>
                     <button disabled class="removeBtn"><i class="fa-regular fa-square-minus"></i></button>
                 </div>
--- dist/js/addInputType.js
+++ dist/js/addInputType.js
@@ -21,19 +21,233 @@ function addSection() {
 
     // Add the HTML content for the new section with unique names
     newSection.innerHTML = `
+            <div class="bind-label">
+            <label for="schedule">Schedule</label>
+
+            <div class="sched-container">
+                <!-- From Label -->
+                <div class="from-label">
+                    <label>From:</label> <!-- Label -->
+                    <div class="main-select-container"> <!-- Selects -->
+                        <!-- Select 1 -->
+                        <div class="select-container">
+                            <select name="schedFromHour${sectionCounter}">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="12">11</option>
+                                <option value="12">12</option>
+                            </select>
+                        </div>
+                        <!-- Select 2 -->
+                        <div class="select-container">
+                            <select name="schedFromMin${sectionCounter}">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                                <option value="25">25</option>
+                                <option value="26">26</option>
+                                <option value="27">27</option>
+                                <option value="28">28</option>
+                                <option value="29">29</option>
+                                <option value="30">30</option>
+                                <option value="31">31</option>
+                                <option value="32">32</option>
+                                <option value="33">33</option>
+                                <option value="34">34</option>
+                                <option value="35">35</option>
+                                <option value="36">36</option>
+                                <option value="37">37</option>
+                                <option value="38">38</option>
+                                <option value="39">39</option>
+                                <option value="40">40</option>
+                                <option value="41">41</option>
+                                <option value="42">42</option>
+                                <option value="43">43</option>
+                                <option value="44">44</option>
+                                <option value="45">45</option>
+                                <option value="46">46</option>
+                                <option value="47">47</option>
+                                <option value="48">48</option>
+                                <option value="49">49</option>
+                                <option value="50">50</option>
+                                <option value="51">51</option>
+                                <option value="52">52</option>
+                                <option value="53">53</option>
+                                <option value="54">54</option>
+                                <option value="55">55</option>
+                                <option value="56">56</option>
+                                <option value="57">57</option>
+                                <option value="58">58</option>
+                                <option value="59">59</option>
+                            </select>
+                        </div>
+                        <!-- Select 3 -->
+                        <div class="select-container">
+                            <select name="schedFromPeriod${sectionCounter}">
+                                <option value="AM">AM</option>
+                                <option value="PM">PM</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- To Label -->
+                <div class="from-label">
+                    <label for="teacherSchedule1">To:</label> <!-- Label -->
+                    <div class="main-select-container"> <!-- Selects -->
+                        <!-- Select 1 -->
+                        <div class="select-container">
+                            <select name="schedToHour${sectionCounter}">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="12">11</option>
+                                <option value="12">12</option>
+                            </select>
+                        </div>
+                        <!-- Select 2 -->
+                        <div class="select-container">
+                            <select name="schedToMin${sectionCounter}">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                                <option value="25">25</option>
+                                <option value="26">26</option>
+                                <option value="27">27</option>
+                                <option value="28">28</option>
+                                <option value="29">29</option>
+                                <option value="30">30</option>
+                                <option value="31">31</option>
+                                <option value="32">32</option>
+                                <option value="33">33</option>
+                                <option value="34">34</option>
+                                <option value="35">35</option>
+                                <option value="36">36</option>
+                                <option value="37">37</option>
+                                <option value="38">38</option>
+                                <option value="39">39</option>
+                                <option value="40">40</option>
+                                <option value="41">41</option>
+                                <option value="42">42</option>
+                                <option value="43">43</option>
+                                <option value="44">44</option>
+                                <option value="45">45</option>
+                                <option value="46">46</option>
+                                <option value="47">47</option>
+                                <option value="48">48</option>
+                                <option value="49">49</option>
+                                <option value="50">50</option>
+                                <option value="51">51</option>
+                                <option value="52">52</option>
+                                <option value="53">53</option>
+                                <option value="54">54</option>
+                                <option value="55">55</option>
+                                <option value="56">56</option>
+                                <option value="57">57</option>
+                                <option value="58">58</option>
+                                <option value="59">59</option>
+                            </select>
+                        </div>
+                        <!-- Select 3 -->
+                        <div class="select-container">
+                            <select name="schedToPeriod${sectionCounter}">
+                                <option value="AM">AM</option>
+                                <option value="PM">PM</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="bind-label">
+            <br>
             <label for="teacherSchedule${sectionCounter}">Schedule:</label>
             <input type="text" id="teacherSchedule${sectionCounter}" name="teacherSchedule${sectionCounter}" required>
         </div>
         <div class="bind-label">
+            <br>
             <label for="teacherSection${sectionCounter}">Section Name:</label>
             <input type="text" id="teacherSection${sectionCounter}" name="teacherSection${sectionCounter}" required>
         </div>
         <div class="bind-label">
+            <br>
             <label for="teacherSubject1${sectionCounter}">Subject Name:</label>
             <input type="text" id="teacherSubject1${sectionCounter}" name="teacherSubject${sectionCounter}" required>
         </div>
+        <!-- School Year -->
+        <div class="bind-label">
+            <label>School Year:</label> <!--Label-->
+            <div class="sched-container"> <!--School Year-->
+                <div class="from-label">
+                    <label>From:</label>
+                    <input type="number" id="teacherSchedule1" name="teacherFromSchoolYear${sectionCounter}">
+                </div>
+                <div class="from-label">
+                    <label>To:</label>
+                    <input type="number" id="teacherSchedule1" name="teacherToSchoolYear${sectionCounter}">
+                </div>
+            </div>
+        </div>
         <div class="withButton">
+            <br>
             <label for="teacherSubject1">d</label>
             <button class="removeBtn" onclick="removeSection(${sectionCounter})"><i class="fa-regular fa-square-minus"></i></button>
         </div>
