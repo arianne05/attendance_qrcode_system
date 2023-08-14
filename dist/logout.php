c8be909b099e6089719e37f3c4288468bdf03a84
arianne05 <quimpoarianne5@gmail.com>
2023-08-14 15:05:53 +0800

    Working login activity for teacher when login and logout

--- dist/logout.php
+++ dist/logout.php
@@ -7,12 +7,24 @@ session_start();
 
 // Perform the logout action
 if (isset($_SESSION['accountID'])) {
+    $accountID = $_SESSION['accountID']; // Store the accountID before unsetting the session
+
     // Clear all session variables
     session_unset();
 
     // Destroy the session
     session_destroy();
 
+    $logLabel = 'log out';
+    $logDate = date('Y-m-d');
+    $logTime = date('H:i:s');
+    
+    // Insert login activity into the database
+    $addLogin = "INSERT INTO login_activity (accountID, logDate, logTime, logLabel) 
+                VALUE (?,?,?,?)";
+    $stmt = $pdo->prepare($addLogin);
+    $stmt->execute([$accountID, $logDate, $logTime, $logLabel]);
+    
     // Redirect the user to the login page or any other desired page
     header("Location: index.php");
     exit();
