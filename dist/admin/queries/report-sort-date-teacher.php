719683e671dc5746c34c595ea75b617f5ed48ca7
arianne05 <quimpoarianne5@gmail.com>
2023-08-14 13:42:49 +0800

    Updated sort query

--- dist/admin/queries/report-sort-date-teacher.php
+++ dist/admin/queries/report-sort-date-teacher.php
@@ -7,35 +7,21 @@ if (isset($_GET['sort_date'])) {
     if (!empty($date_from) && empty($date_to)) {
         // Totals
         $total_prof = $pdo->query("SELECT COALESCE(COUNT(*), 0) FROM account_information WHERE position='teacher' AND dateAdded = '$date_from'")->fetchColumn();
-        $total_students = $pdo->query("SELECT COALESCE(COUNT(*), 0) FROM student WHERE dateAdded = '$date_from'")->fetchColumn();
-        $total_users = $total_prof + $total_students;
-
-        $student = 'student';
-
+    
         // Fetch login_act table and student table
-        $stmt = $pdo->prepare("SELECT accountID, firstname, middlename, lastname, sex, position, dateAdded FROM account_information WHERE position='teacher' AND dateAdded = '$date_from'
-                            UNION ALL
-                            SELECT studentNumber, firstname, middlename, lastname, studentGender, :student, dateAdded FROM $student WHERE dateAdded = '$date_from'");
-        $stmt->bindParam(':student', $student, PDO::PARAM_STR);
+        $stmt = $pdo->prepare("SELECT * FROM account_information WHERE position='teacher' AND dateAdded = '$date_from'");
         $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $teacherGet = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
     }
     if (!empty($date_from) && !empty($date_to)) { //WHEN BOTH DATE IS SELECTED
         // Totals
         $total_prof = $pdo->query("SELECT COALESCE(COUNT(*), 0) FROM account_information WHERE position='teacher' AND dateAdded >= '$date_from' AND dateAdded <= '$date_to'")->fetchColumn();
-        $total_students = $pdo->query("SELECT COALESCE(COUNT(*), 0) FROM student WHERE dateAdded >= '$date_from' AND dateAdded <= '$date_to'")->fetchColumn();
-        $total_users = $total_prof + $total_students;
-
-        $student = 'student';
 
         // Fetch login_act table and student table
-        $stmt = $pdo->prepare("SELECT accountID, firstname, middlename, lastname, sex, position, dateAdded FROM account_information WHERE position='teacher' AND dateAdded >= '$date_from' AND dateAdded <= '$date_to'
-                            UNION ALL
-                            SELECT studentNumber, firstname, middlename, lastname, studentGender, :student, dateAdded FROM $student WHERE dateAdded >= '$date_from' AND dateAdded <= '$date_to'");
-        $stmt->bindParam(':student', $student, PDO::PARAM_STR);
+        $stmt = $pdo->prepare("SELECT * FROM account_information WHERE position='teacher' AND dateAdded >= '$date_from' AND dateAdded <= '$date_to'");
         $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $teacherGet = $stmt->fetchAll(PDO::FETCH_ASSOC);
        
     }
 } else { //WHEN THERE ARE NO SELECTED DATE
